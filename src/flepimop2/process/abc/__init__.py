13a1582e817cdac9640d801d825e742dbb62c3d8
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-11-25 11:03:18 -0500

    Add validation hook to `ProcessABC`
    Added a hook to incorporate validation into a process action. This
    validation hook only runs in dry mode and is optional, however if not
    provided it is assumed that the user has incorporated dry mode into
    their regular execute method themselves. 
    
    - Added `flepimop2.exceptions` subpackage with a generic
      `Flepimop2Error` and more specific `Flepimop2ValidationError` which
      takes a list of `ValidationIssues` to produce a pydantic-esque
      exception.
    - Added detailed API reference documentation for the
      `flepimop2.exceptions` subpackage.
    - Added `ProcessABC._process_validate` hook to provide a way for
      `ProcessABC` subclasses to provide validation without running.
    
    Addresses #25 for `flepimop2.process`.

--- src/flepimop2/process/abc/__init__.py
+++ src/flepimop2/process/abc/__init__.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from flepimop2._utils._module import _load_builder, _resolve_module_name
 from flepimop2.configuration import ModuleModel
+from flepimop2.exceptions import Flepimop2ValidationError, ValidationIssue
 
 
 class ProcessABC(ABC):
@@ -17,13 +18,30 @@ class ProcessABC(ABC):
         Args:
             dry_run: If True, the process will not actually execute but will simulate
                 execution.
+
+        Raises:
+            Flepimop2ValidationError: If validation fails during a dry run.
         """
+        if dry_run and (result := self._process_validate()) is not None:
+            if result:
+                raise Flepimop2ValidationError(result)
+            return None
         return self._process(dry_run=dry_run)
 
     @abstractmethod
     def _process(self, *, dry_run: bool) -> None:
         """Backend-specific implementation for processing data."""
         ...
+
+    def _process_validate(self) -> list[ValidationIssue] | None:  # noqa: PLR6301
+        """
+        Process validation hook.
+
+        Returns:
+            A boolean indicating if the process is valid, or `None` if not implemented.
+
+        """
+        return None
 
 
 def build(config: dict[str, Any] | ModuleModel) -> ProcessABC:
