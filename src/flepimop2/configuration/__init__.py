07e76046b463e0608489f53611c39f61baf3a4ab
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2026-02-19 16:08:30 -0500

    Add `axes` top level key to configuration format
    Added the concept of 'axes' to represent a single dimension for
    something that has shape. Axes can take the form of categorical like
    'unvaccinated' and 'vaccinated' or 'low', 'medium', 'high' as well as
    numerical forms like physical space being represented by a continuous
    axis. This provides a foundation for further structuring parameters and
    systems by giving them shape.
    
    - Added `axes` attribute to the `ConfigurationModel` class to contain a
      mapping of named axes that can be used throughout a configuration
      file, but accessing this results in a `DeprecationWarning` because
      there is no functionality implemented currently.
    - Added `CategoricalAxisModel` and `ContinuousAxisModel` classes to
      represent the two different kinds of axis provided by `flepimop2`.
      These classes represent the configuration form of these axes objects,
      including pydantic based validations. 
    
    Heavily inspired by work from @MacdonaldJoshuaCaleb on
    `ACCIDDA/op_system`. Closes #147.

--- src/flepimop2/configuration/__init__.py
+++ src/flepimop2/configuration/__init__.py
@@ -1,13 +1,23 @@
 """Representations of parsed configuration files."""
 
 __all__ = [
+    "AxesGroupModel",
+    "AxisModel",
+    "CategoricalAxisModel",
     "ConfigurationModel",
+    "ContinuousAxisModel",
     "IdentifierString",
     "ModuleGroupModel",
     "ModuleModel",
     "SimulateSpecificationModel",
 ]
 
+from flepimop2.configuration._axes import (
+    AxesGroupModel,
+    AxisModel,
+    CategoricalAxisModel,
+    ContinuousAxisModel,
+)
 from flepimop2.configuration._configuration import ConfigurationModel
 from flepimop2.configuration._module import ModuleGroupModel, ModuleModel
 from flepimop2.configuration._simulate import SimulateSpecificationModel
